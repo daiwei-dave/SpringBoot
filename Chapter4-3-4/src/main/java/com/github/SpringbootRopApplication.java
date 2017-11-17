60efeb7cf5acad3dd717c8cb04c91d4fbdbf7ad8
daiwei <18428385839@163.com>
2017-11-17 14:53:09 +0800

    springboot-rop项目搭建

---
+++ Chapter4-3-4/src/main/java/com/github/SpringbootRopApplication.java
@@ -0,0 +1,12 @@
+package com.github;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRopApplication.class, args);
+	}
+}
